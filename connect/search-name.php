7d1fc3d924733b3e7a128eb2dfdcd4de280601a9
lim44104 <s10207455@connect.np.edu.sg>
2020-12-22 18:32:18 +0800

    testapi

--- connect/search-name.php
+++ connect/search-name.php
@@ -7,8 +7,8 @@ if (isset($_GET['name'])) {
   $name_to_search = htmlentities(strtolower($_GET['name'])); // HuLk == hulk
 
   $ts = time();
-  $public_key = c9bd7f4fdaa53eaaf8e9601f929b99d9;
-  $private_key = b4a68d0f961d62ce4f7f0a2d53917be811e8dccc;
+  $public_key = "c9bd7f4fdaa53eaaf8e9601f929b99d9";
+  $private_key = "b4a68d0f961d62ce4f7f0a2d53917be811e8dccc";
   $hash = md5($ts . $private_key . $public_key);
 
   $query = array(
